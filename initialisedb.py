5d565060c7dce6dbdf62bf762f41b61496aedf17
David Young <david@thedavidyoung.co.uk>
2017-08-31 13:54:59 +0000

    create blank tables for data and topics

--- initialisedb.py
+++ initialisedb.py
@@ -2,6 +2,7 @@
 
 import config
 import psycopg2
+import psycopg2.extras
 
 try:
 	conn = psycopg2.connect(
@@ -13,8 +14,9 @@ try:
 except:
 	print("Error connecting to database")
 
-cur = conn.cursor()
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
+print("Dropping old tables...")
 try:
 	cur.execute("DROP TABLE IF EXISTS %s"%config.TABLE_DATA)
 except:
@@ -24,3 +26,27 @@ try:
 	cur.execute("DROP TABLE IF EXISTS %s"%config.TABLE_TOPICS)
 except:
 	print("Cannot drop table '%s'"%config.TABLE_TOPICS)
+
+print("Creating fresh tables...")
+
+try:
+	cur.execute("""
+		CREATE TABLE """ + config.TABLE_DATA + """ (
+			id SERIAL PRIMARY KEY,
+			topic INTEGER NOT NULL,
+			data jsonb,
+			time timestamptz NOT NULL DEFAULT now()
+		);""")
+except:
+	print("Error creating table " + config.TABLE_DATA)
+
+try:
+	cur.execute("""
+		CREATE TABLE """ + config.TABLE_TOPICS + """ (
+			id SERIAL PRIMARY KEY,
+			name VARCHAR(255) NOT NULL
+		);""")
+except:
+	print("Error creating table " + config.TABLE_TOPICS)
+
+print("Success!")
